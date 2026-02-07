49bddefda580b51038bdb62f6c8669e1239201c7
Henrique Torres <henriquetorres92@gmail.com>
2026-02-07 10:00:55 -0300

    Add Flip sprite to face character correct movement position

--- Assets/Scripts/Runtime/Manager/States/Character/ActionCharacterState.cs
+++ Assets/Scripts/Runtime/Manager/States/Character/ActionCharacterState.cs
@@ -11,6 +11,7 @@ namespace Assets.Scripts.Runtime.Manager.States.Character
 
         private Vector2 _movementInput;
         private InputSystem_Actions inputActions = new InputSystem_Actions();
+        private bool facingRight = false;
 
         public Transform CharacterTransform;
 
@@ -28,6 +29,20 @@ namespace Assets.Scripts.Runtime.Manager.States.Character
 
         protected override void OnUpdate()
         {
+            if (_movementInput.x > 0 && !facingRight)
+            {
+                Flip(true);
+            }
+            else if (_movementInput.x < 0 && facingRight)
+            {
+                Flip(false);
+            }
+
+            if (_movementInput.magnitude > 0f)
+            {
+                // Animator move parameter set true;
+            }
+
             CharacterTransform.Translate(new Vector3(_movementInput.x, _movementInput.y, 0) * Time.deltaTime * 5f);
         }
 
@@ -52,6 +67,15 @@ namespace Assets.Scripts.Runtime.Manager.States.Character
         void OnMove(InputAction.CallbackContext ctx)
         {
             _movementInput = ctx.ReadValue<Vector2>();
+        }
+
+        void Flip(bool faceRight)
+        {
+            facingRight = faceRight;
+
+            Vector3 scale = CharacterTransform.localScale;
+            scale.x = faceRight ? -1 : 1;
+            CharacterTransform.localScale = scale;
         }
     }
 }
